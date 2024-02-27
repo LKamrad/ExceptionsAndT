3180458ccf60c7845cae860fbd2f38bae1fa691f
Livia Kamrad <Kwadrat777@gmail.com>
2024-02-27 15:45:37 +0100

    wtf

---
+++ Baum/Interfaces/Interfaces.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baum
+{
+    public interface INode<T> : IEnumerable<T>
+    {
+        T Value { get; }
+        INode<T> Add(T nodeValue);
+        IEnumerable<INode<T>> Children { get; }
+        IEnumerable<T> ChildValues { get; }
+        IEnumerable<T> PreOrderValues();
+        IEnumerable<T> PostOrderValues();
+    }
+    
+    public interface ITree<T>
+        where T : IComparable
+    {
+        INode<T> AddToParent(T parentValue, T value);
+        bool Contains(T Value);
+        IEnumerable<T> PreOrderValues();
+        IEnumerable<T> PostOrderValues();
+    }
+
+}
---
+++ Baum/Models/Tree.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baum
+{
+    class Tree<T> : ITree<T>
+        where T : IComparable
+    {
+        public  INode<T> _parent;
+
+        public Tree(T input)
+        {
+
+            Node<T> _temp = new Node<T>()
+            {
+                Value = input
+            };
+
+            _parent = _temp;
+
+        }
+        public INode<T> AddToParent(T parentValue, T value)
+        {
+
+            INode<T> temp = Search(parentValue);
+
+            if(temp != null)
+            {
+                return temp.Add(value);
+            }
+            throw new InvalidOperationException();
+        }
+
+        public INode<T> Search(T value)
+        {
+            if(_parent.Value.CompareTo(value) == 0)
+            {
+                return _parent;
+            }
+            return SearchChild(_parent, value);
+        }
+        public INode<T> SearchChild(INode<T> node, T value)
+        {
+            INode<T> temp = null;
+            foreach (var child in node.Children)
+            {
+                if (child.Children != null)
+                {
+                    temp = SearchChild(child, value);
+                    if(temp != null)
+                    {
+                        return temp;
+                    }
+                }
+                if (child.Value.CompareTo(value) == 0)
+                {
+
+                    temp = child;
+                    return temp;
+                }
+            }
+
+            return temp;
+        }
+
+
+        public bool Contains(T Value)
+        {
+            foreach(var pv in PostOrderValues())
+            {
+                if (pv.CompareTo(Value) == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public IEnumerable<T> PostOrderValues()
+        {
+            return _parent.PostOrderValues();
+        }
+
+        public IEnumerable<T> PreOrderValues()
+        {
+            return _parent.PreOrderValues();
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+}
